dc09f0f9075d54e5ff436c6d2392dd1d5bfdacdc
Agastya Krothapalli <agastyakro06@gmail.com>
2025-04-24 20:26:36 -0400

    Satellite.hpp built out

--- Satellite.hpp
+++ Satellite.hpp
@@ -5,8 +5,25 @@ class Satellite {
     public:
         Satellite();
 
-        void applyTorque(const Eigen::Vector3d& torque, double dt);
-        
+        void applyTorque(const Eigen::Vector3d& torque, double dt); // gets our angular velocity
+        void update(double dt); // updates the satellite orientation 
+
+        void setTargetOrientation(const Eigen::Quaterniond& target);
+        Eigen::Quaterniond getOrientation() const;
+        Eigen::Vector3d getAngularVelocity() const;
+
+    private:
+
+        Eigen::Quaterniond orientation; // current orientation
+        Eigen::Vector3d angular_velocity;
+        Eigen::Matrix3d inertia;
+        Eigen::Quaterniond target_orientation;
+
+        Eigen::Vector3d computeControlTorque(); // PID output
+
+
+
+
 
 
 
